346def298d3b7a65ee0da3fb2ef79a5019268f0a
Kevin Chisholm <kevin@kevinchisholm.com>
2016-11-13 05:49:20 -0500

    use a for-loop to imitate dynamic back-end

--- example-3.js
+++ example-3.js
@@ -5,15 +5,19 @@ var express = require('express'),
 
 //tell express what to do when the / route is requested
 app.get('/', function (req, res) {
+  var i = 1,
+    max = 5;
+
   //set the appropriate HTTP header
   res.setHeader('Content-Type', 'text/html');
 
-  //send a response to the client
-  res.write("<h1>This is the first response</h1>");
-  //send a second response to the client
-  res.write("<h2>This is the second response</h2>");
-  //send a third response to the client
-  res.end("<h3>This is the third response</h3>");
+  //send multiple responses to the client
+  for (; i <= max; i++) {
+    res.write('<h1>This is the response #: ' + i + '</h1>');
+  }
+
+  //end the response process
+  res.end();
 });
 
 //wait for a connection
