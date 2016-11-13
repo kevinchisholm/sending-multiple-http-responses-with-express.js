f651dac4c725650787af6c313732c3e3e7634d71
Kevin Chisholm <kevin@kevinchisholm.com>
2016-11-13 05:14:30 -0500

    Add example-2.js

---
+++ example-2.js
@@ -0,0 +1,22 @@
+//require the express nodejs module
+var express = require('express'),
+  //set an instance of exress
+  app = express();
+
+//tell express what to do when the / route is requested
+app.get('/', function (req, res) {
+  //set the appropriate HTTP header
+  res.setHeader('Content-Type', 'text/html');
+
+  //send a response to the client
+  res.write("<h1>This is the first response</h1>");
+  //send a second response to the client
+  res.write("<h2>This is the second response</h2>");
+  //send a third response to the client
+  res.write("<h3>This is the third response</h3>");
+});
+
+//wait for a connection
+app.listen(5000, function () {
+  console.log('The web server is running. Please open http://localhost:5000/ in your browser.');
+});
